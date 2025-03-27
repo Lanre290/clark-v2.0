e0b43bc05b51a29e3d1a9060616408118ccd5da5
segunojo1 <lanre2967@gmail.com>
2025-03-27 16:41:46 +0100

    feat: Waitlist mail added

--- src/Controllers/WaitlistController.ts
+++ src/Controllers/WaitlistController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import userWaitlist from "../Models/Waitlist";
+import { sendWaitlistMail } from "../Mailing/waitlistWelcome";
 
 interface waitListInterface {
   addUser: (req: Request, res: Response) => Promise<Response | void>;
@@ -26,6 +27,7 @@ const waitlistActions: waitListInterface = {
       }
 
       await userWaitlist.create({ name, email});
+      sendWaitlistMail(email, name);
 
       return res
         .status(201)
