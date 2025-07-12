8e946b5d94e3012d68cba0145441c2f6895abe7f
segunojo1 <lanre2967@gmail.com>
2025-07-12 05:29:31 +0100

    refactor: Change sequelize.sync to force sync for database reset during development

--- src/script.ts
+++ src/script.ts
@@ -115,7 +115,7 @@ const startServer = async () => {
     await sequelize.authenticate();
     console.log("DB connection established");
 
-    await sequelize.sync({ alter: true });
+    await sequelize.sync({ force: true });
     console.log("Database synced");
 
     app.listen(PORT, () => {
