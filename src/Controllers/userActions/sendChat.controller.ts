048f14eda87d4b43a3277d81fb984b5a62c785ec
Lanre290 <lanre2967@gmail.com>
2026-04-05 03:04:53 +0100

    feat: enhance sendChat controller to include chat_title in response and improve file handling logic

--- src/Controllers/userActions/sendChat.controller.ts
+++ src/Controllers/userActions/sendChat.controller.ts
@@ -9,27 +9,52 @@ import Messages from "../../Models/Message";
 import { processFiles } from "../../utils/fileHandler.utils";
 import { ai } from "../../Services/gemini.services";
 
+interface ConfigInterface {
+  responseMimeType: string;
+  responseSchema: {
+    type: string;
+    properties: {
+      answer: { type: string; description: string };
+      chat_title?: { type: string; description: string };
+    };
+    required: string[];
+  };
+}
+
 export const sendChat = async (
-    req: Request & afterVerificationMiddlerwareInterface,
-    res: Response
-  ) => {
-    try {
-      let { chat_id, text, previous_messages, strict_mode } = req.body;
-      const user = req.user;
-      const files = req.files;
+  req: Request & afterVerificationMiddlerwareInterface,
+  res: Response,
+) => {
+  try {
+    let { chat_id, text, previous_messages, strict_mode } = req.body;
+    const user = req.user;
+    const files = req.files;
+    let aiConfig: ConfigInterface = {
+      responseMimeType: "application/json",
+      responseSchema: {
+        type: "object",
+        properties: {
+          answer: {
+            type: "string",
+            description: "The answer to the question asked.",
+          },
+        },
+        required: ["answer"],
+      },
+    };
 
-      if (!user || !user.id) {
-        return res.status(401).json({ error: "Unauthorized access." });
-      }
+    if (!user || !user.id) {
+      return res.status(401).json({ error: "Unauthorized access." });
+    }
 
-      if (files && files != undefined && (files.length as number) > 10) {
-        return res.status(400).json({
-          error: "Bad request",
-          message: "File to be uploaded at the same time cannot exceed 10.",
-        });
-      }
+    if (files && files != undefined && (files.length as number) > 10) {
+      return res.status(400).json({
+        error: "Bad request",
+        message: "File to be uploaded at the same time cannot exceed 10.",
+      });
+    }
 
-      const prompt = `You are a highly intelligent assistant. Please answer the user's question using the following rules:
+    const prompt = `You are a highly intelligent assistant. Please answer the user's question using the following rules:
 
                       1. If any files are provided, use them as your **primary source**.
                       2. If no files are provided but there is a previous conversation, use that as context.
@@ -51,8 +76,8 @@ export const sendChat = async (
                         files?.length
                           ? `🗂️ Files have been provided. Use them as your primary reference.`
                           : previous_messages
-                          ? `💬 Previous conversation:\n${previous_messages}`
-                          : `❗ No prior context is available.`
+                            ? `💬 Previous conversation:\n${previous_messages}`
+                            : `❗ No prior context is available.`
                       }
 
                       ---
@@ -63,131 +88,154 @@ export const sendChat = async (
                       ${text}
                       `;
 
-      // create chat if its not provided.
-      if (!chat_id) {
-        await Chats.create({
-          userId: user.id,
-        }).then((chat) => {
-          chat_id = chat.id;
-        });
-      }
-
-      let parts: any[] = [];
-      parts.push({ text: prompt });
-
-      const filesArray: { url: string } & Express.Multer.File[] = [] as any;
-
-      const pdfFiles = await PDFFiles.findAll({
-        where: { chatId: chat_id, workspaceId: null },
-        attributes: ["filePath"],
+    // create chat if its not provided.
+    if (!chat_id) {
+      await Chats.create({
+        userId: user.id,
+      }).then((chat) => {
+        chat_id = chat.id;
       });
 
-      const imageFiles = await ImageFiles.findAll({
-        where: { chatId: chat_id, workspaceId: null },
-        attributes: ["filePath"],
-      });
+      aiConfig = {
+        responseMimeType: "application/json",
+        responseSchema: {
+          type: "object",
+          properties: {
+            answer: {
+              type: "string",
+              description: "The answer to the question asked.",
+            },
+            chat_title: {
+              type: "string",
+              description:
+                "a suggested name for this conversation, this is the first question in this chat with you, you would be supplied context for follow questions.",
+            },
+          },
+          required: ["answer", "chat_title"],
+        },
+      };
+    }
 
-      if (files !== undefined && (files.length as number) > 0) {
-        const uploadPromises = (files as Express.Multer.File[]).map(
-          async (file) => {
-            let key = `${Date.now()}_${file.originalname}`;
+    let parts: any[] = [];
+    parts.push({ text: prompt });
 
-            key = key.replace(/[^a-zA-Z0-9.]/g, "_");
-            const bucket = `chat_${chat_id}`;
+    const filesArray: { url: string } & Express.Multer.File[] = [] as any;
 
-            const url = `https://${process.env.R2_ENDPOINT_DOMAIN}/${bucket}/${key}`;
-            await uploadFile(bucket, bucket, key, file.buffer, file.mimetype);
+    const pdfFiles = await PDFFiles.findAll({
+      where: { chatId: chat_id, workspaceId: null },
+      attributes: ["filePath"],
+    });
 
-            if (file.mimetype.includes("pdf")) {
-              await PDFFiles.create({
-                fileName: file.originalname,
-                chatId: chat_id,
-                userId: user.id,
-                filePath: url,
-                size: formatFileSize(file.size),
-              });
-            } else if (file.mimetype.includes("image")) {
-              await ImageFiles.create({
-                fileName: file.originalname,
-                size: formatFileSize(file.size),
-                chatId: chat_id,
-                userId: user.id,
-                filePath: url,
-              });
-            }
+    const imageFiles = await ImageFiles.findAll({
+      where: { chatId: chat_id, workspaceId: null },
+      attributes: ["filePath"],
+    });
 
-            const newFile = {
-              ...file,
-              url,
-            } as Express.Multer.File & { url: string };
+    if (files !== undefined && (files.length as number) > 0) {
+      const uploadPromises = (files as Express.Multer.File[]).map(
+        async (file) => {
+          let key = `${Date.now()}_${file.originalname}`;
 
-            filesArray.push(newFile);
+          key = key.replace(/[^a-zA-Z0-9.]/g, "_");
+          const bucket = `chat_${chat_id}`;
 
-            return newFile;
-          }
-        );
+          const url = `https://${process.env.R2_ENDPOINT_DOMAIN}/${bucket}/${key}`;
+          await uploadFile(bucket, bucket, key, file.buffer, file.mimetype);
 
-        const processedFiles = await Promise.all(uploadPromises);
-
-        processedFiles.forEach(async (file) => {
-          await Messages.create(
-            {
-              text: file.originalname,
+          if (file.mimetype.includes("pdf")) {
+            await PDFFiles.create({
+              fileName: file.originalname,
               chatId: chat_id,
-              fromUser: true,
-              isFile: true,
-              filePath: file.url,
+              userId: user.id,
+              filePath: url,
               size: formatFileSize(file.size),
             });
-        });
+          } else if (file.mimetype.includes("image")) {
+            await ImageFiles.create({
+              fileName: file.originalname,
+              size: formatFileSize(file.size),
+              chatId: chat_id,
+              userId: user.id,
+              filePath: url,
+            });
+          }
 
-        await Messages.create({
-          text,
-          chatId: chat_id,
-          fromUser: true,
-          isFile: false,
-        });
-      } else {
-        await Messages.create({
-          text: text,
-          chatId: chat_id,
-          fromUser: true,
-        });
-      }
+          const newFile = {
+            ...file,
+            url,
+          } as Express.Multer.File & { url: string };
 
-      parts = await processFiles(parts, pdfFiles, imageFiles);
+          filesArray.push(newFile);
 
-      // add the current uploaded files
-      parts.push(
-        ...(files as Express.Multer.File[]).map((file) => ({
-          inlineData: {
-            mimeType: file.mimetype,
-            data: Buffer.from(file.buffer).toString("base64"),
-          },
-        }))
+          return newFile;
+        },
       );
 
-      const response = await ai.models.generateContent({
-        model: process.env.REGULAR_MODEL as string,
-        contents: [
-          {
-            role: "user",
-            parts: parts,
-          },
-        ],
-      });
+      const processedFiles = await Promise.all(uploadPromises);
 
-      const answer = response.text;
+      processedFiles.forEach(async (file) => {
+        await Messages.create({
+          text: file.originalname,
+          chatId: chat_id,
+          fromUser: true,
+          isFile: true,
+          filePath: file.url,
+          size: formatFileSize(file.size),
+        });
+      });
 
       await Messages.create({
-        text: answer,
+        text,
         chatId: chat_id,
-        fromUser: false,
+        fromUser: true,
+        isFile: false,
       });
-
-      return res.status(200).json({ answer, chat_id });
-    } catch (error) {
-      console.log(error)
-      res.status(500).json({ error: "Server error." });
+    } else {
+      await Messages.create({
+        text: text,
+        chatId: chat_id,
+        fromUser: true,
+      });
     }
+
+    parts = await processFiles(parts, pdfFiles, imageFiles);
+
+    // add the current uploaded files
+    parts.push(
+      ...(files as Express.Multer.File[]).map((file) => ({
+        inlineData: {
+          mimeType: file.mimetype,
+          data: Buffer.from(file.buffer).toString("base64"),
+        },
+      })),
+    );
+
+    const response = await ai.models.generateContent({
+      model: process.env.REGULAR_MODEL as string,
+      contents: [
+        {
+          role: "user",
+          parts: parts,
+        },
+      ],
+      config: aiConfig as any,
+    });
+
+    const json = JSON.parse(response.text as string);
+
+    if (json.chat_title) {
+      Chats.update({ name: json.chat_title }, { where: { id: chat_id } });
+    }
+
+    await Messages.create({
+      text: json.answer,
+      chatId: chat_id,
+      fromUser: false,
+    });
+
+    return res.status(200).json({ answer: json.answer, chat_id, chat_title: json.chat_title });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
   }
+};
